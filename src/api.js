7326bbc91d5da5a82248391e6aacf61a86be4171
eriick505 <santoserick9@gmail.com>
2021-04-01 08:41:21 -0300

    condicional de exibição das evoluções dos pokemons em desenvolvimento

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
-const API = 'https://pokeapi.co/api/v2/pokemon'
+const API = "https://pokeapi.co/api/v2/pokemon";
 
-const fetchData = (_, index) => fetch(`${API}/${index + 130}`)
-  .then(response => response.json())
+const fetchData = (_, index) =>
+  fetch(`${API}/${index + 120}`).then((response) => response.json());
 
-const fetchPokemonsPromises = () => Array(5).fill('').map(fetchData)
+const fetchPokemonsPromises = () => Array(15).fill("").map(fetchData);
 
 const getAllPokemons = async () => {
-  const allPokemons = await Promise.all(fetchPokemonsPromises())
-  return allPokemons
-}
+  const allPokemons = await Promise.all(fetchPokemonsPromises());
+  return allPokemons;
+};
 
 // const getPokemonSpeciesById = async (id) => {
 //   const allPokemons = await getAllPokemons()
@@ -19,79 +19,84 @@ const getAllPokemons = async () => {
 //     .then(r => r.json()))
 
 //   const speciesData = await Promise.all(speciesPromises)
-  
+
 //   return speciesData[id - 1]
 // }
 
 const getByIdPokemonSpecie = async (id) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
-  return await response.json()
-}
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
+  );
+  return await response.json();
+};
 
 const getEvolutionChainsById = async (id) => {
   const specieData = await getByIdPokemonSpecie(id);
 
-  const evolutionChainsUrl = specieData.evolution_chain.url
+  const evolutionChainsUrl = specieData.evolution_chain.url;
 
   const getEvolutionChainsData = async () => {
-    const response = await fetch(evolutionChainsUrl)
-    return await response.json()
-  }
+    const response = await fetch(evolutionChainsUrl);
+    return await response.json();
+  };
 
-  const evochainsData = await getEvolutionChainsData()
+  const evochainsData = await getEvolutionChainsData();
 
   const createObjectEvoChains = (data) => {
-    let evoChains = []
-    let evoData = data.chain
-    let evolutionTo = evoData.evolves_to
+    let evoChains = [];
+    let evoData = data.chain;
+    let evolutionTo = evoData.evolves_to;
 
     do {
-      let numberOfEvolutions = evoData.evolves_to.length
-      let evoDetails = evoData.evolution_details[0]
+      let numberOfEvolutions = evoData.evolves_to.length;
+      let evoDetails = evoData.evolution_details[0];
 
       evoChains.push({
-        "species_name": evoData.species.name,
-        "min_level": !evoDetails ? 1 : evoDetails.min_level,
-        "trigger_name": !evoDetails ? null : evoDetails.trigger.name,
-        "item": !evoDetails ? null : evoDetails.item
-      })
+        species_name: evoData.species.name,
+        min_level: !evoDetails ? 1 : evoDetails.min_level,
+        trigger_name: !evoDetails ? null : evoDetails.trigger.name,
+        item: !evoDetails ? null : evoDetails.item,
+      });
 
-      if(numberOfEvolutions > 1) {
+      if (numberOfEvolutions > 1) {
         evolutionTo.map((evo, index) => {
-          const {evolution_details, species} = evo
+          const { evolution_details, species } = evo;
 
-          if(index !== 0) {
-            evoChains.push({
-              "species_name": !species ? null : species.name,
-              "min_level": !evolution_details.length ? 1 : evolution_details[0].min_level,
-              "item": !evolution_details.length ? null : evolution_details[0].item?.name 
-            })
-            return
+          if (index === 0) {
+            return null;
           }
-        })
+
+          return evoChains.push({
+            species_name: !species ? null : species.name,
+            min_level: !evolution_details.length
+              ? 1
+              : evolution_details[0].min_level,
+            item: !evolution_details.length
+              ? null
+              : evolution_details[0].item?.name,
+          });
+        });
       }
-  
-      evoData = evoData.evolves_to[0]
 
-    } while(!!evoData && evoData.hasOwnProperty('evolves_to'))
-    console.log(evoChains);
-    return evoChains
+      evoData = evoData.evolves_to[0];
+    } while (!!evoData && evoData.hasOwnProperty("evolves_to"));
+    return evoChains;
+  };
+
+  return createObjectEvoChains(evochainsData);
+};
+
+const getPokemonByNameOrId = async (nameOrId) => {
+  if (nameOrId) {
+    const response = await fetch(`${API}/${nameOrId}`);
+    return await response.json();
   }
-
-  return createObjectEvoChains(evochainsData)
-}
-
-const getPokemonByNameOrId = async nameOrId => {
-  if(nameOrId) {
-    const response = await fetch(`${API}/${nameOrId}`)
-    return await response.json()
-  }
-}
+};
 
 export {
   API,
   getAllPokemons,
   // getPokemonSpeciesById,
   getEvolutionChainsById,
-  getPokemonByNameOrId
-}
+  getPokemonByNameOrId,
+};
--- src/components/Modal/Evolutions/index.js
+++ src/components/Modal/Evolutions/index.js
@@ -1,67 +1,85 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import './evolutions.css'
-import { usePokemons } from '../../../Context/Pokedex'
-import { getEvolutionChainsById, getPokemonByNameOrId } from '../../../api'
+import styles from "./evolutions.module.css";
+import { usePokemons } from "../../../Context/Pokedex";
+import { getEvolutionChainsById, getPokemonByNameOrId } from "../../../api";
 
 const Evolutions = ({ pokemonId, color }) => {
-  const [evoChains, setEvoChains] = useState([])
-  const [pokeImgId, setPokeImgId] = useState([])
-  const { pokemons, } = usePokemons()
+  const [evoChains, setEvoChains] = useState([]);
+  const [pokeImgId, setPokeImgId] = useState([]);
+  const { pokemons } = usePokemons();
 
   useEffect(() => {
     const evolutionChain = async () => {
-      const data = await getEvolutionChainsById(pokemonId)
-      setEvoChains(data) 
-    }
-    evolutionChain()
-  }, [setEvoChains, pokemonId])
+      const data = await getEvolutionChainsById(pokemonId);
+      setEvoChains(data);
+    };
+    evolutionChain();
+  }, [setEvoChains, pokemonId]);
 
   useEffect(() => {
     const getPokemonID = async () => {
-      const pokemonId = evoChains.map(async chain => {
-        const pokemonFiltered = pokemons.filter(pokemon => 
-          pokemon.name === chain.species_name)
+      const pokemonId = evoChains.map(async (chain) => {
+        const pokemonFiltered = pokemons.filter(
+          (pokemon) => pokemon.name === chain.species_name
+        );
 
-        if(!pokemonFiltered.length) {
-          const pokemonData = await getPokemonByNameOrId(chain.species_name)
-          pokemonFiltered.push(pokemonData)
+        if (!pokemonFiltered.length) {
+          const pokemonData = await getPokemonByNameOrId(chain.species_name);
+          pokemonFiltered.push(pokemonData);
         }
 
-        return pokemonFiltered[0].id
-      })
+        return pokemonFiltered[0].id;
+      });
 
-      const allPromisesPokemon = await Promise.all(pokemonId)
-      setPokeImgId(allPromisesPokemon)
-      
-    }
-    getPokemonID()
-  }, [evoChains, pokemons])
-    
-  const imgUrl = id => `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+      const allPromisesPokemon = await Promise.all(pokemonId);
+      setPokeImgId(allPromisesPokemon);
+    };
+    getPokemonID();
+  }, [evoChains, pokemons]);
 
+  const imgUrl = (id) =>
+    `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+
+  if (evoChains.length <= 1)
+    return (
+      <div className={styles.evoContainer}>
+        <p>Este pokemon não possui evoluções 😔</p>
+      </div>
+    );
   return (
-    <div>
-      <ul className="evolutionList">
-        {evoChains.map((poke, index) => (
-          <li key={poke.species_name} className="evolutionItem">
-            <div className="evolutionBoxImg">
-              <img
-                src={imgUrl(pokeImgId[index])} 
-                alt={poke.species_name}
-              />
-            </div>
-            <h5>{poke.species_name}</h5>
-            {poke.min_level && 
-              <span 
-                className={"level " + (color)}>
-                Lv. {poke.min_level}
-              </span>}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+    <div className={styles.evoContainer}>
+      {evoChains.length <= 3 ? (
+        <ul className={styles.evolutionList}>
+          {evoChains.map((poke, index) => (
+            <li key={poke.species_name} className={styles.evolutionItem}>
+              <div className={styles.evolutionBoxImg}>
+                <img src={imgUrl(pokeImgId[index])} alt={poke.species_name} />
+              </div>
+              <h5>{poke.species_name}</h5>
+              {poke.min_level && (
+                <span className={`${styles.level} ${color}`}>
+                  Lv. {poke.min_level}
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <>
+          {evoChains.map((poke, index) => {
+            if (index <= 4 && index !== 0) {
+              return <h5>{poke.species_name}</h5>;
+            }
 
-export default Evolutions
+            if (index === 0) {
+              return <h3>{poke.species_name}</h3>;
+            }
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Evolutions;
