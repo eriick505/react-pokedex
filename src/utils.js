ea27169c8b9ba90274aee405f2315562ac607ec4
unknown <santoserick@gmail.com>
2021-04-13 20:43:43 -0300

    Mudança na função 'getNecessaryDataFromEvoChains' para retornar um objeto com os dados necessarios da cadeia de evolução, criada uma função recursiva removendo a utilização de do/while da implementação anterior

--- src/utils.js
+++ src/utils.js
@@ -3,49 +3,27 @@ const pokemonTypesAsArray = ({ types }) => types.map(({ type }) => type.name);
 const getPokemonImageById = id =>
   `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
 
-const getNecessaryDataFromEvoChains = data => {
-  let evoChains = [];
-  let evoData = data.chain;
-  let evolutionTo = evoData.evolves_to;
+const getNecessaryDataFromEvoChains = ({ species, evolution_details }) => ({
+  species_name: species.name,
+  min_level: !evolution_details.length ? 1 : evolution_details[0].min_level,
+  trigger_name: !evolution_details.length
+    ? null
+    : evolution_details[0].trigger.name,
+  item: !evolution_details.length ? null : evolution_details.item,
+});
 
-  do {
-    let numberOfEvolutions = evoData.evolves_to.length;
-    let evoDetails = evoData.evolution_details[0];
+function getAllEvolutions(data) {
+  let storageArray = [];
 
-    evoChains.push({
-      species_name: evoData.species.name,
-      min_level: !evoDetails ? 1 : evoDetails.min_level,
-      trigger_name: !evoDetails ? null : evoDetails.trigger.name,
-      item: !evoDetails ? null : evoDetails.item,
-    });
+  storageArray.push(getNecessaryDataFromEvoChains(data));
 
-    if (numberOfEvolutions > 1) {
-      evolutionTo.map((evo, index) => {
-        const { evolution_details, species } = evo;
+  let nextEvolution = data.evolves_to;
 
-        if (index === 0) {
-          return null;
-        }
+  nextEvolution.map(evolution => {
+    return (storageArray = storageArray.concat(getAllEvolutions(evolution)));
+  });
 
-        return evoChains.push({
-          species_name: !species ? null : species.name,
-          min_level: !evolution_details.length
-            ? 1
-            : evolution_details[0].min_level,
-          item: !evolution_details.length
-            ? null
-            : evolution_details[0].item?.name,
-        });
-      });
-    }
+  return storageArray;
+}
 
-    evoData = evoData.evolves_to[0];
-  } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
-  return evoChains;
-};
-
-export {
-  pokemonTypesAsArray,
-  getPokemonImageById,
-  getNecessaryDataFromEvoChains,
-};
+export { pokemonTypesAsArray, getPokemonImageById, getAllEvolutions };
