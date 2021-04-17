1f311ae2242a78a03a99abd8c1b8a4728effc26b
unknown <santoserick@gmail.com>
2021-04-17 12:06:22 -0300

    mudança no componente header para receber as props que serão passadas para o componente searchBar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,7 +3,7 @@ import SearchBar from '../SearchBar';
 import logoPokemon from '../../assets/img/pokemon.png';
 import { header, logo, searchContainer } from './Header.module.css';
 
-export default function Header() {
+export default function Header({ setSearchPokemon, setFoundPokemon }) {
   return (
     <header className={header}>
       <div className={logo}>
@@ -11,7 +11,10 @@ export default function Header() {
       </div>
 
       <div className={searchContainer}>
-        <SearchBar />
+        <SearchBar
+          setSearchPokemon={setSearchPokemon}
+          setFoundPokemon={setFoundPokemon}
+        />
       </div>
     </header>
   );
