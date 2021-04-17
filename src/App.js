b161a8ca6b97bc81ace11be323b673f8527fcd1f
unknown <santoserick@gmail.com>
2021-04-17 11:54:23 -0300

    criado os estados searchPokemon: para receber o pokemon pesquisado, e o foundPokemon: para fazer a mudança da exibição dos componentes de resultado de pesquisa e a pokedex

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import Footer from './Components/Footer';
 import Pokedex from './Components/Pokedex';
 import Modal from './Components/Modal';
 import './App.css';
+import SearchResult from './Components/SearchResult';
 
 function App() {
   const [pokemonModal, setPokemonModal] = React.useState(null);
+  const [searchPokemon, setSearchPokemon] = React.useState(null);
+  const [foundPokemon, setFoundPokemon] = React.useState(false);
 
   return (
     <>
-      <Header />
-      <Pokedex setPokemonModal={setPokemonModal} />
+      <Header
+        setSearchPokemon={setSearchPokemon}
+        setFoundPokemon={setFoundPokemon}
+      />
+
+      {foundPokemon ? (
+        <SearchResult
+          pokemon={searchPokemon}
+          setPokemonModal={setPokemonModal}
+        />
+      ) : (
+        <Pokedex setPokemonModal={setPokemonModal} />
+      )}
+
       {pokemonModal && (
         <Modal pokemonModal={pokemonModal} setPokemonModal={setPokemonModal} />
       )}
