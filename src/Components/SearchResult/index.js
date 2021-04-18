a2533ea6f34f89cba846930a48d281682b5518c0
unknown <santoserick@gmail.com>
2021-04-18 13:40:37 -0300

    Remoção da props pokemon e adicionado o contexto que contem o pokemon encontrado na pesquisa

--- src/Components/SearchResult/index.js
+++ src/Components/SearchResult/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { SearchContext } from '../../Context/SearchPokemon';
 import Card from '../Card';
 import { container, title } from './SearchResult.module.css';
 
-function SearchResult({ pokemon, setPokemonModal }) {
+function SearchResult({ setPokemonModal }) {
+  const { searchPokemon: pokemon } = React.useContext(SearchContext);
+
   return (
     <div className={container}>
       <h2 className={title}>
